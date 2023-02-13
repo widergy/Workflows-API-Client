e09ed44737b5650685452abf82d24fba9a79412f
JoaquinSeita <joaquin.seita@widergy.com>
2023-02-13 18:11:19 -0300

    implement test improvements

--- spec/lib/workflows_api_client_spec.rb
+++ spec/lib/workflows_api_client_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe WorkflowsApiClient do
-  describe '.workflows_index' do
-    let(:utility_id) { 123 }
+  let(:utility_id) { 123 }
+  let(:worker_class) { WorkflowsApiClient::WorkflowsIndexByUtilityWorker }
 
+  describe '.workflows_index' do
     it 'creates a new instance of WorkflowsIndexByUtilityWorker' do
-      expect(WorkflowsApiClient::WorkflowsIndexByUtilityWorker).to receive(:new).and_call_original
+      expect(worker_class).to receive(:new).and_call_original
       described_class.workflows_index(utility_id)
     end
   end
 
   describe '.request_headers' do
-    let(:utility_id) { 123 }
+    let(:headers) { described_class.request_headers(utility_id) }
 
     it 'returns the expected request headers' do
-      headers = described_class.request_headers(utility_id)
       expect(headers).to eq({ headers: {
-                              'Utility-ID': '123',
+                              'Utility-ID': utility_id.to_s,
                               'Content-Type': 'application/json'
                             } })
     end
