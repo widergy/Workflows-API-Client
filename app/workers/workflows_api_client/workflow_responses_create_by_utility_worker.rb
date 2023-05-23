0ba1ab11012eda5cc5f7fba55e750d450389e75f
Marcio Garozzo <marcio.garozzo@widergy.com>
2023-05-23 17:32:15 -0300

    feat: update workflow responses create worker

--- app/workers/workflows_api_client/workflow_responses_create_by_utility_worker.rb
+++ app/workers/workflows_api_client/workflow_responses_create_by_utility_worker.rb
@@ -2,17 +2,25 @@ module WorkflowsApiClient
   class WorkflowResponsesCreateByUtilityWorker < BaseWorker
     private
 
+    def user_id
+      @user_id ||= params[:body_params][:consumer_user_id]
+    end
+
     def account_id
-      @account_id ||= params[:body_params].extract!(:account_id)[:account_id]
+      @account_id ||= params[:body_params][:consumer_account_id]
+    end
+
+    def user
+      @user ||= user_id.present? ? User.find(user_id) : nil
     end
 
     def account
-      account_id.present? ? Account.find(account_id) : nil
+      account_id.present? ? user.accounts.find(account_id) : nil
     end
 
-    def account_ids
+    def utility_ids
       {
-        consumer_account_id: account&.id,
+        utility_user_id: user&.external_id,
         utility_account_id: account&.external_id
       }.compact
     end
@@ -21,7 +29,7 @@ module WorkflowsApiClient
       {
         http_method: :post,
         url: BASE_URL + WORKFLOW_RESPONSES_SERVICE_URL,
-        body_params: params[:body_params].merge(account_ids)&.to_json
+        body_params: params[:body_params].merge(utility_ids)&.to_json
       }
     end
   end
