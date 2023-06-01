43230a529c5e4a152091be47b37ba59ee6b4a461
Marcio Garozzo <marcio.garozzo@widergy.com>
2023-06-01 15:14:08 -0300

    feat: use inherit to errorbuilderhelper

--- app/helpers/workflows_api_client/error_builder_helper.rb
+++ app/helpers/workflows_api_client/error_builder_helper.rb
@@ -1,7 +1,9 @@
 module WorkflowsApiClient
   module ErrorBuilderHelper
-    error_builder = "::#{WorkflowsApiClient.config[:error_builder]}".safe_constantize
-    raise Exceptions::InvalidConfiguration, 'error_builder' if error_builder.blank?
-    error_builder
+    def self.inherit
+      error_builder = "::#{WorkflowsApiClient.config[:error_builder]}".safe_constantize
+      raise Exceptions::InvalidConfiguration, 'error_builder' if error_builder.blank?
+      error_builder
+    end
   end
 end
