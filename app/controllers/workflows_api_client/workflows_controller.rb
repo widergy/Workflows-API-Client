2d9a27b8efa41b8509466c48b3bea11512b4c45d
Joaquín Hetrea <joaquin.hetrea@widergy.com>
2026-05-15 15:31:06 -0300

    fix: rename method

--- app/controllers/workflows_api_client/workflows_controller.rb
+++ app/controllers/workflows_api_client/workflows_controller.rb
@@ -2,7 +2,7 @@ module WorkflowsApiClient
   class WorkflowsController < ApplicationBaseController
     skip_before_action :authenticate_request,
                        only: WorkflowsApiClient.config[:skip_auth_workflows_services]
-    before_action :validate_captcha!, if: :should_validate_captcha?, only: %i[show]
+    before_action :validate_captcha, if: :captcha_required?, only: %i[show]
 
     include AsyncRequestHelper
 
